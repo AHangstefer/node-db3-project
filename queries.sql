278820cc30f4cba6be835926dc343511fb1550a0
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-10-20 20:26:12 -0400

    one query to go

--- queries.sql
+++ queries.sql
@@ -1,6 +1,7 @@
 -- Multi-Table Query Practice
 
--- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
+-- Display the ProductName and CategoryName for all products in the database. 
+--Shows 77 records.
 SELECT Product.Id, Product.ProductName, Category.CategoryName
 FROM Product 
 JOIN Category ON Product.CategoryId = Category.Id;
@@ -13,6 +14,19 @@ FROM "Order"
 JOIN "Shipper" ON "Order"."ShipVia" = "Shipper"."Id"
 WHERE "OrderDate" < "2012-08-09";
 
--- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+-- Display the name and quantity of the products ordered in order with
+-- Id 10251. Sort by ProductName. Shows 3 records.
+SELECT
+    "OrderDetail"."OrderId",
+    "OrderDetail"."ProductId",
+    "Product"."ProductName"
+FROM
+    "OrderDetail"
+JOIN "Product" ON "OrderDetail"."ProductId" = "Product"."Id"
+WHERE
+    "OrderId" = "10251"
+ORDER BY "ProductName";
 
--- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+-- Display the OrderID, Customer's Company Name and the employee's LastName 
+--for every order. All columns should be labeled clearly. Displays 16,789 
+--records.
