0fe47db5e818e57fb37ab28813ade96678841098
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-10-21 11:23:55 -0400

    setting up scheme-module function

---
+++ schemes/scheme-model.js
@@ -0,0 +1,42 @@
+
+const db = require("../data/config")
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+}
+
+
+function find() {
+
+}
+
+
+function findById(){
+
+}
+
+
+function findSteps(id){
+
+}
+
+function add(scheme){
+
+}
+
+
+function update(changes,id){
+
+
+}
+
+function remove(id){
+
+    
+}
+
